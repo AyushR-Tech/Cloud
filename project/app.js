343b4812da4097539356460121035c8448d9ba33
Ayush Rewatkar <137433346+AyushR-Tech@users.noreply.github.com>
2025-04-15 09:47:10 +0530

    Update app.js

--- project/app.js
+++ project/app.js
@@ -2,7 +2,7 @@ const express = require('express');
  const app = express();
  const port = 3000;
  app.get('/', (req, res) => {
- res.send('Hello, World!');
+ res.send('After applying changes');
  });
  app.listen(port, () => {
  console.log(`App is running on http://localhost:${port}`);
